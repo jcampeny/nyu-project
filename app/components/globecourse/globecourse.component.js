ea221ea73d3bd7aed8a520f0ed562c62af643058
Jordi Campeny <jcampeny@elkanodata.com>
2016-07-05 11:28:33 +0200

    Revisiones:  Como se puede editar texto general de GLOBE Course

--- app/components/globecourse/globecourse.component.js
+++ app/components/globecourse/globecourse.component.js
@@ -3,11 +3,14 @@ angular.module('app').directive('nyuGlobecourse', function () {
     restrict: 'E',
     templateUrl: '../app/components/globecourse/globecourse.html',
     controllerAs: 'nyuMediakit',
-    controller: function ($scope, EntitiesService) {
+    controller: function ($scope, EntitiesService, DataService, $sce) {
     	$scope.entity = "globecourse";
-
+        var slug = 'gloube-course';
+        $scope.content = '';
         $scope.entityLabels = EntitiesService.getEntityLabels(); 
-
+        DataService.all('pages', '', '', false, '?slug='+slug).then(function(page){
+            $scope.content = page[0].content;
+        });
         $scope.groupItems = function(){
             return EntitiesService.groupItems($scope.entity);
         };
