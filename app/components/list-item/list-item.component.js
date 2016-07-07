764b653735156ec6b5f8a8a2f6e963ea0f0a2574
Jordi Campeny <jcampeny@elkanodata.com>
2016-07-07 11:51:10 +0200

    Revisiones: Play/pause Podcasts

--- app/components/list-item/list-item.component.js
+++ app/components/list-item/list-item.component.js
@@ -64,8 +64,15 @@ angular.module('app').directive('nyuListItem', function ($timeout) {
     				$scope.other !== "";
     	};
     	$scope.audioPlaying = false;
-    	$scope.toggleAudio = function(){
+    	$scope.toggleAudio = function(id){
+            $animation = $('#'+id);
+            var pause = "M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26";
+            var play = "M11,10 L18,13.74 18,22.28 11,26 M18,13.74 L26,18 26,18 18,22.28";
     		$scope.audioPlaying = !$scope.audioPlaying;
+            $animation.attr({
+               "from": $scope.audioPlaying ? pause : play,
+               "to": $scope.audioPlaying ? play : pause
+            }).get(0).beginElement();
     	};
 
     	$scope.getTitleUrl = function(){
