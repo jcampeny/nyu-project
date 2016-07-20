29047a99bfbd3113b533a99b929fec2df1a05de5
Jordi Campeny <jcampeny@elkanodata.com>
2016-07-20 19:26:57 +0200

    Custom URL: Funciona sin el post2post, más libertad sin errores

--- app/components/custom-view/view.component.js
+++ app/components/custom-view/view.component.js
@@ -1,4 +1,4 @@
-angular.module('app').directive('customView', function ($stateParams, DataService, $state, $sce) {
+angular.module('app').directive('customView', function ($stateParams, DataService, $state, $sce, $location) {
     return {
         restrict: 'E',
         template: '',
@@ -9,17 +9,21 @@ angular.module('app').directive('customView', function ($stateParams, DataServic
                 id : ''
             };
             var found = false;
+            var url = '';
             DataService.getFriendlyUrl().then(function(posts){
                 angular.forEach(posts, function(postItem){
+                
                     if($sce.valueOf(postItem.title) == $stateParams.id){
                         found =  true;
-                        state.title = postItem.postsrelated[0].post_title;
-                        state.type = postItem.postsrelated[0].post_type;
-                        state.id = postItem.postsrelated[0].ID;  
+                        url = DataService.htmlToPlaintext($sce.valueOf(postItem.content));
+                        //state.title = postItem.postsrelated[0].post_title;
+                        //state.type = postItem.postsrelated[0].post_type;
+                        //state.id = postItem.postsrelated[0].ID;  
                     }
                 });
                 if(found) {
-                    $state.go("app."+state.type+'item', {id: state.id, title: state.title});
+                    //$state.go("app."+state.type+'item', {id: state.id, title: state.title});
+                    $location.path(url);
                 }else{
                     $state.go("app.notfound");
                 }
