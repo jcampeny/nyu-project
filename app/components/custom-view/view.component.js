465b9591d856bae92ee4fb741938c7439ed37a26
Jordi Campeny <jcampeny@elkanodata.com>
2016-07-27 16:13:29 +0200

    Custom view funciona correctamente

--- app/components/custom-view/view.component.js
+++ app/components/custom-view/view.component.js
@@ -9,21 +9,21 @@ angular.module('app').directive('customView', function ($stateParams, DataServic
                 id : ''
             };
             var found = false;
-            var url = '';
+            var newUrl = '';
             DataService.getFriendlyUrl().then(function(posts){
                 angular.forEach(posts, function(postItem){
-                
                     if($sce.valueOf(postItem.title) == $stateParams.id){
                         found =  true;
-                        url = DataService.htmlToPlaintext($sce.valueOf(postItem.content));
+                        
+                        newUrl = DataService.htmlToPlaintext($sce.valueOf(postItem.content));
+
                         //state.title = postItem.postsrelated[0].post_title;
                         //state.type = postItem.postsrelated[0].post_type;
                         //state.id = postItem.postsrelated[0].ID;  
                     }
                 });
                 if(found) {
-                    //$state.go("app."+state.type+'item', {id: state.id, title: state.title});
-                    $location.path(url);
+                    $location.path(newUrl.replace(/(\r\n|\n|\r)/gm,""));
                 }else{
                     $state.go("app.notfound");
                 }
