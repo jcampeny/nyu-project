3dbd2c7847479e6bf400d111b4fa832b6fbb3248
Jordi Campeny <jcampeny@elkanodata.com>
2016-09-09 15:23:15 +0200

    User subscriptions: refactor

--- php/user-subscriptions/user_subscriptions.php
+++ php/user-subscriptions/user_subscriptions.php
@@ -14,15 +14,16 @@ Author URI: http://elkanodata.com/
 
 function user_subscriptions_admin(){
 	include('user_subscriptions_admin_view.php');
-
+	
 }
  
 function user_subscriptions() {
- 	add_menu_page("Subscriptions", "Subscriptions", 1, "Subscriptions", "user_subscriptions_admin", 'dashicons-groups');
+ 	add_menu_page("Subscriptions", "Subscriptions", 1, "Subscriptions", "user_subscriptions_admin", 'dashicons-tickets-alt');
+
 }
 
 add_action('admin_menu', 'user_subscriptions');
-
 require_once('ajax_calls.php');
+
 
 ?>
