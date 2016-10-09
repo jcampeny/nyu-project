f3c2b5516980fed757cb7751490daec0ab5e912c
Cristiam Da Silva <cristiam86@gmail.com>
2016-10-09 11:33:19 +0200

    [WIP] Select directive with database results

---
+++ app/components/other/dd-item-array/dd-item-array.component.js
@@ -0,0 +1,88 @@
+angular.module('app').directive('ddItemArray', function () {
+  return {
+    restrict: 'E',
+    templateUrl: '../app/components/other/dd-item-array/dd-item-array.html',
+    controllerAs: 'ddItemArray',
+    scope : {
+    	variables : '=',
+        result : '='
+    },
+    controller: function ($scope, LoginService, $http, $rootScope) {
+        $scope.dropDownCollapsed = "";
+
+        $scope.toggleCollapsed = function(scopeKey, newValue){
+            $scope[scopeKey] = newValue; 
+        };
+
+    },
+    link: function (s, e, a){
+
+        function refreshResult(){
+            s.result.items = [];
+            angular.forEach(s.variables, function(parentValue, parentKey){
+                angular.forEach(parentValue, function(value, key){
+                    angular.forEach(value, function(childValue, childKey){
+                        if($('input[name="'+key+childValue.name+'"]').prop('checked')){
+                            childValue.default = true;
+                            s.result.items.push({
+                                parent : key,
+                                name   : childValue.name
+                            });
+                        }
+                        
+                    });
+                });
+            });
+        }
+
+        s.parentSelect = function(parentKey, key){
+            var parentObj = $('input[name="'+key+'"]');
+            var newState = !(!parentObj.prop("checked"));
+
+            angular.forEach(s.variables[parentKey][key], function(itemValue, itemKey){
+                $('input[name="'+key+itemValue.name+'"]').prop('checked', newState);
+            });
+
+            parentObj.prop('checked', newState);
+            refreshResult();
+        };
+
+        s.childSelect = function(parentKey, key){
+
+            var parentObj = $('input[name="'+key+'"]');
+            var parentClass = new ParentController();
+
+            function ParentController (){
+                this.childs = [];
+                this.allChildSame = function () {
+                    for(var i = 1; i < this.childs.length; i++)
+                    {
+                        if(this.childs[i] !== this.childs[0])
+                            return false;
+                    }
+                    return true;
+                };
+            }  
+
+            parentObj.prop({
+                checked : false,
+                indeterminate : false
+            });
+
+
+            angular.forEach(s.variables[parentKey][key], function(itemValue, itemKey){
+                parentClass.childs.push($('input[name="'+ key + itemValue.name+'"]').prop('checked'));
+            });
+
+            if(parentClass.allChildSame()){
+                parentObj.prop('checked', parentClass.childs[0]);
+            }else{
+                parentObj.prop('indeterminate', true);
+            }
+            
+            refreshResult();
+        };
+
+    }
+  };
+});
